03f87da16e040afb66a5afaa1a3a878f60fbd93b
LingNinghuang <138959332+LingNinghuang@users.noreply.github.com>
2023-07-09 14:23:04 +0800

    Create Ex1-6–6.cpp

---
+++ Ex1-6–6.cpp
@@ -0,0 +1,13 @@
+//variables
+#include <stdio.h>
+int main(){
+    int x = 11;
+    int y = 24;
+    int sum = x + y;
+    printf("%d\n", sum);
+    int a = 3, b = 21, c = 7;
+    printf("%d\n", a + b + c);
+    int d, e, f;
+    d = e = f = 69;
+    printf("%d",d + e + f);
+}
